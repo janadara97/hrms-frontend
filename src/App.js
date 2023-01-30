b32bf56dca281be1940bb22de391f2e4fad3dd8f
Chathura Janadara <cjanadara@isa.ae>
2023-01-30 23:37:56 +0530

    redux-init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './assets/styles/main.scss';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 
 function App() {
   return (
-    <></>
+    <Provider store={store}>
+      <></>
+    </Provider>
   );
 }
 
